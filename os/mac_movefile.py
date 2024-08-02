1f6f8d67ac704c4c2739afb2486de26c97fe36b2
hARRi <tranhoangtd@gmail.com>
2024-08-03 06:05:33 +0700

    1

---
+++ os/mac_movefile.py
@@ -0,0 +1,10 @@
+import shutil
+import os
+
+#success!
+
+from1 = '/Users/minhhoangtran/REPOSITORY/settings/os/ping2.txt'
+to1 = '/Users/minhhoangtran/Desktop/'
+
+shutil.move(from1, to1)
+print(f"File moved")
